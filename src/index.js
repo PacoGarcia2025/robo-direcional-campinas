36cf985670e621234ff93b13557aa507e0c0efea
PacoGarcia2025 <sgoliveira16@gmail.com>
2025-12-26 09:43:04 -0300

    Refactor main function to use IIFE

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-// src/index.js
 import runDirecional from "./robots/direcional.js";
-import enrichDirecional from "./enrich/index.js";
 import generateXml from "./generateXml.js";
 
-async function main() {
-  console.log("🚀 Iniciando Robô Direcional Campinas");
-
-  await runDirecional();
-  enrichDirecional();
-  generateXml(
-    "src/output/direcional-enriched.json",
-    "src/output/direcional-campinas.xml"
-  );
-}
-
-main();
+(async () => {
+  const empreendimentos = await runDirecional();
+  generateXml(empreendimentos, "src/output/direcional.xml");
+})();
