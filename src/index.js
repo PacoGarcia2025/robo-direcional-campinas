c2648449b3914b5f7202511354e597112926a08b
PacoGarcia2025 <sgoliveira16@gmail.com>
2025-12-26 08:13:38 -0300

    Add console log for starting Direcional Robot

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+// src/index.js
 import runDirecional from "./robots/direcional.js";
 import enrichDirecional from "./enrich/index.js";
 import generateXml from "./generateXml.js";
 
 async function main() {
+  console.log("🚀 Iniciando Robô Direcional Campinas");
+
   await runDirecional();
   enrichDirecional();
   generateXml(
