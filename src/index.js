752ff53261c2447117641bafabf5d0761f7fe45d
PacoGarcia2025 <sgoliveira16@gmail.com>
2025-12-27 13:15:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import runDirecional from "./robots/direcional.js";
+import extractDirecional from "./robots/direcional.extractor.js";
+import generateBaseJson from "./generateBaseJson.js";
 import generateXml from "./generateXml.js";
 import generateXmlX09 from "./generateXml.x09.js";
 
 (async () => {
-  const empreendimentos = await runDirecional();
+  const data = await extractDirecional();
 
-  console.log(
-    "🔎 SAMPLE EMPREENDIMENTO:",
-    JSON.stringify(empreendimentos[0], null, 2)
-  );
-
-  generateXml(empreendimentos);
-  generateXmlX09(empreendimentos);
+  generateBaseJson(data);
+  generateXml(data);
+  generateXmlX09(data);
 })();
