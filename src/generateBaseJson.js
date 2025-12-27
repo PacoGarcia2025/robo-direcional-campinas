7149222688b35f76e9a152ed63c38cadae7d7555
PacoGarcia2025 <sgoliveira16@gmail.com>
2025-12-27 13:12:57 -0300

    Update generateBaseJson.js

--- src/generateBaseJson.js
+++ src/generateBaseJson.js
@@ -1,1 +1,9 @@
+import fs from "fs";
+import path from "path";
+
+export default function generateBaseJson(data) {
+  const filePath = path.resolve("src/output/direcional.base.json");
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
+  console.log("✅ Base JSON gerada:", data.length, "empreendimentos");
+}
 
