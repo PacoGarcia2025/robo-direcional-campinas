9bc7333387e1242f6341ab26eab993143b23f44a
PacoGarcia2025 <sgoliveira16@gmail.com>
2025-12-27 02:37:31 -0300

    Update generateXml.x09.js

--- src/generateXml.x09.js
+++ src/generateXml.x09.js
@@ -1,6 +1,9 @@
 import fs from "fs";
 import path from "path";
 
+const FOTO_FALLBACK =
+  "https://www.direcional.com.br/wp-content/themes/direcional-theme/dist/images/logo-direcional.svg";
+
 export default function generateXmlX09(empreendimentos) {
   let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<empreendimentos>\n`;
 
@@ -19,16 +22,23 @@ export default function generateXmlX09(empreendimentos) {
     xml += `  <empreendimento>\n`;
     xml += `    <id>${id}</id>\n`;
     xml += `    <titulo>${emp.titulo || "Empreendimento Direcional"}</titulo>\n`;
+    xml += `    <tipo>Apartamento</tipo>\n`;
     xml += `    <cidade>${cidade}</cidade>\n`;
     xml += `    <estado>${estado}</estado>\n`;
     xml += `    <status>${emp.status || "Lançamento"}</status>\n`;
 
     xml += `    <fotos>\n`;
-    (emp.imagens || []).forEach((img) => {
-      xml += `      <foto>${img}</foto>\n`;
-    });
-    xml += `    </fotos>\n`;
 
+    if (emp.imagens && emp.imagens.length > 0) {
+      emp.imagens.forEach((img) => {
+        xml += `      <foto>${img}</foto>\n`;
+      });
+    } else {
+      // 🔒 FOTO OBRIGATÓRIA PARA O X09
+      xml += `      <foto>${FOTO_FALLBACK}</foto>\n`;
+    }
+
+    xml += `    </fotos>\n`;
     xml += `  </empreendimento>\n`;
   });
 
@@ -41,6 +51,6 @@ export default function generateXmlX09(empreendimentos) {
 
   fs.writeFileSync(filePath, xml, "utf8");
   console.log(
-    `📦 XML X09 gerado com sucesso (${empreendimentos.length} itens): ${filePath}`
+    `📦 XML X09 gerado com sucesso (${empreendimentos.length} empreendimentos)`
   );
 }
