e97b7bc884ff4411834211c93fec171059419711
PacoGarcia2025 <sgoliveira16@gmail.com>
2025-12-28 21:57:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import extractDirecional from "./robots/direcional.extractor.FINAL.js";
+import extractMRV from "./robots/mrv.extractor.js";
+
 import generateBaseJson from "./generateBaseJson.js";
 import generateXml from "./generateXml.js";
 import generateXmlX09 from "./generateXml.x09.js";
 
 (async () => {
-  const data = await extractDirecional();
+  // ============================
+  // DIRECIONAL
+  // ============================
+  console.log("===== INICIANDO DIRECIONAL =====");
+  const direcionalData = await extractDirecional();
 
-  generateBaseJson(data);
-  generateXml(data);
-  generateXmlX09(data);
+  generateBaseJson(direcionalData, "direcional");
+  generateXml(direcionalData, "direcional");
+  generateXmlX09(direcionalData, "direcional");
+
+  // ============================
+  // MRV
+  // ============================
+  console.log("===== INICIANDO MRV =====");
+  const mrvData = await extractMRV();
+
+  generateBaseJson(mrvData, "mrv");
+  generateXml(mrvData, "mrv");
+  generateXmlX09(mrvData, "mrv");
 })();
